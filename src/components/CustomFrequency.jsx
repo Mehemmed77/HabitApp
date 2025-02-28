eadfb8329fc389ef7e6c625772bc965e5812e48f
Mehemmed Kazimzade <mehemmedkazimzade54@gmail.com>
2025-02-28 23:52:21 +0400

    aaa

--- src/components/CustomFrequency.jsx
+++ src/components/CustomFrequency.jsx
@@ -1,18 +1,22 @@
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
 import InputLabel from '@mui/material/InputLabel';
+import Slider from '@mui/material/Slider';
 import {useState} from "react";
 
 export default function CustomFrequency() {
     const [value, setValue] = useState("");
+    const [form, setForm] = useState(<> </>);
 
     const handleChange = (e) => {
         const value = e.target.value;
         setValue(value);
 
         if(value === 1) {
-
+            setForm(<Slider sx={{width: "200px"}} aria-label="Days" defaultValue={4} valueLabelDisplay="auto" step={1} marks min={2} max={7}/>)
         }
+        if(value === 2) setForm(<></>);
+        if (value === 3) setForm(<Slider sx={{width: "300px"}} aria-label="Days" defaultValue={10} valueLabelDisplay="auto" step={1} marks min={5} max={40}/>);
     }
 
     return <>
@@ -22,5 +26,6 @@ export default function CustomFrequency() {
             <MenuItem value={2}>Indefinitely</MenuItem>
             <MenuItem value={3}>Stop after X occurrence</MenuItem>
         </Select>
+        {form}
     </>
 }
